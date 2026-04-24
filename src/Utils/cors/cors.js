3a3f068dc9d99d0f072cde3472f7ba2fc4bbd4a5
Eslam Elawdy <eslam.mohamed.ki123@gmail.com>
2026-04-25 02:02:53 +0300

    hotfix

--- src/Utils/cors/cors.js
+++ src/Utils/cors/cors.js
@@ -1,6 +1,6 @@
 import { Accessple_Origines } from "../../../config/config.service.js";
 
-const Origines = Accessple_Origines.split(",");
+const Origines = Accessple_Origines.split(",") || [];
 export default function OriginesCors() {
   return {
     origin: function (origin, callback) {
@@ -8,6 +8,8 @@ export default function OriginesCors() {
         console.log(origin);
         return callback(null, origin);
       } else if (!origin) {
+        return callback(null, origin);
+      } else {
         return callback(null, origin);
       }
       return callback(new Error("unothorized origin"), null);
